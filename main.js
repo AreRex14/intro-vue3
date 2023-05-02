8923bc83a0b98c7ec6748b6832a92f7ff7f4e06f
Arif Z <22223756+AreRex14@users.noreply.github.com>
2023-05-03 04:12:10 +0800

    create vue app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Sandals',
+            description: 'A pair of elegant Christmas sandals'
+        }
+    }
+})
